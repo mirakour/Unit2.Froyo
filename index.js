0f3bd4c4b1522269dd470ab881e9a46201d646e1
mirakour <mirandakourt@gmail.com>
2025-02-27 14:39:26 -0800

    Update index.js
    updated/debugged

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ const userInput = prompt(
   );
   
   // Convert the user input into an array of flavors.
-  const flavorArray = userInput.split(",");
+  const flavorArray = userInput.split(",")
   
   // Display froyo flavors slected by user.
   console.log("The Froyo flavors selected are:", userInput)
 
   // Function to count the occurrences of each flavor
   function countFlavors(flavorList) {
-    const flavorCounts = {};
+    const flavorCounts = {}
   
     for (let i = 0; i < flavorList.length; i++) {
       const flavor = flavorList[i].trim().toLowerCase(); // Normalize input
       if (flavorCounts[flavor]) {
-        flavorCounts[flavor]++;
+        flavorCounts[flavor]++
       } else {
-        flavorCounts[flavor] = 1;
+        flavorCounts[flavor] = 1
       }
     }
   
@@ -28,7 +28,7 @@ const userInput = prompt(
   }
   
   // Get the counted flavor orders
-  const flavorCounts = countFlavors(flavorArray);
+  const flavorCounts = countFlavors(flavorArray)
   
   // Display the results in the console as a table
   console.table(flavorCounts);
